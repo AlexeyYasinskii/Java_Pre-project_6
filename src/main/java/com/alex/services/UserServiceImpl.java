32038fd7047037d041c2171d09e32e7ba18a9a75
AlexeyYasinskii <alexey.yasinskiy93@gmail.com>
2024-08-29 21:03:29 +0300

    task 2.3.1 is done

---
+++ src/main/java/com/alex/services/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.alex.services;
+
+import com.alex.dao.UserDAO;
+import com.alex.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    @Transactional
+    public List<User> showAllUsers() {
+        return userDAO.showAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        userDAO.saveUser(user);
+    }
+
+    @Override
+    @Transactional
+    public User showUser(int id) {
+        return userDAO.showUser(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User user) {
+        userDAO.updateUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(int id) {
+        userDAO.deleteUser(id);
+    }
+}
